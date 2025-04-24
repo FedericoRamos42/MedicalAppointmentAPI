64985545bb293772261d2c03a3f07f628ec2b431
Lorenzo Martinez <151043241+Martinez17s@users.noreply.github.com>
2025-04-24 20:55:59 -0300

    Feature/create-specialty

---
+++ src/Application/Mappers/SpecialtyMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using Domain.Entities;
+
+namespace Application.Mappers
+{
+    public static class SpecialtyMapper
+    {
+        public static SpecialtyDto ToDto(this Specialty specialty) => new SpecialtyDto
+        {
+            Id = specialty.Id,
+            Name = specialty.Name,
+            Description = specialty.Description
+        };
+        public static List<SpecialtyDto> ToListDto(this IEnumerable<Specialty> specialties) => specialties.Select(x => ToDto(x)).ToList();
+    }
+}
---
+++ src/Application/Services/SpecialtyService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.Mappers;
+using Application.Models;
+using Application.Models.Request;
+using Application.Result;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.Services
+{
+    public class SpecialtyService : ISpecialtyService
+    {
+        private readonly ISpecialtyRepository _repository;
+        public SpecialtyService(ISpecialtyRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<Result<SpecialtyDto>> Create(SpecialtyCreateRequest request)
+        {
+            Specialty specialty = new Specialty()
+            {
+                Name = request.Name,
+                Description = request.Description
+            };
+            await _repository.AddAsync(specialty);
+            var dto = specialty.ToDto();
+            return Result<SpecialtyDto>.Success(dto);
+        }
+        public Task<Result<SpecialtyDto>> Delete(int id)
+        {
+
+        }
+        public Task<Result<IEnumerable<SpecialtyDto>>> GetAll()
+        {
+            
+        }
+        public Task<Result<SpecialtyDto>> GetById(int id)
+        {
+            
+        }
+    }
+}
